f525663b9b5a44d5a57af39120314bf185c1ee37
Rubén Blázquez Camacho <rblazquez@outlook.com>
2019-03-29 11:43:03 +0100

    ok

---
+++ STORAGEQUEUES/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage.Queue;
+using STORAGE_QUEUES.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace STORAGEQUEUES.Controllers
+{
+   
+    public class HomeController : Controller
+    {
+        
+        RepositoryQueues repo;
+        List<CloudQueueMessage> mensajes;
+        public HomeController()
+        {
+            this.repo = new RepositoryQueues();
+        }
+
+        public ActionResult Index()
+        {
+            //todos los nombres de las queue
+            ViewBag.colas = this.repo.GetCloudQueue();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index( String mensaje, String nombre)
+        {
+            //todos los nombres de las queue
+            ViewBag.colas = this.repo.GetCloudQueue();
+
+           //preguntamos si el campo viene vacio 
+                 if(mensaje != "") { 
+
+                    String subir =  mensaje + " " + DateTime.Now.ToShortTimeString();
+                    this.repo.CrearMensaje(nombre, subir);
+                 }
+           // recibo todos los mensajes
+            mensajes = this.repo.GetMensajes(nombre);
+            
+            return View(mensajes);
+        }
+       
+        public ActionResult CrearNuevaCola()
+        {
+            //todos los nombres de las queue
+            List<String> colas = this.repo.GetCloudQueue();
+            return View(colas);
+        }
+
+        [HttpPost]
+        public ActionResult CrearNuevaCola(String nombre)
+        {
+            //creo la nueva queue
+            CloudQueue lista = this.repo.Createcola(nombre);
+
+            //todos los nombres de las queue
+            List<String> colas = this.repo.GetCloudQueue();
+
+            return View(colas);
+        }
+
+        public ActionResult Deletecola(String cola)
+        {
+            //eliminamos una queue por el nombre
+            this.repo.EliminarMensajes(cola);
+
+            return RedirectToAction("CrearNuevaCola");
+        }
+
+
+    }
+}
---
+++ STORAGEQUEUES/Repositories/RepositoryQueues.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STORAGE_QUEUES.Repositories
+{
+    public class RepositoryQueues
+    {
+        CloudQueueClient client;
+        public RepositoryQueues()
+        {
+            //llamamos al nombre de la cuenta de almacenamiento en web.conf
+            String keys = CloudConfigurationManager.GetSetting("cuentaazure");
+            CloudStorageAccount account = CloudStorageAccount.Parse(keys);
+            //Creamos un cliente para la cola
+            this.client = account.CreateCloudQueueClient();
+
+        }
+
+        public CloudQueue Createcola(String name)
+        {
+            //Recuperamos el contenedor de la cola
+            //de cada programador
+            CloudQueue queue = this.client.GetQueueReference(name);
+            //creamos la cola nueva
+            queue.CreateIfNotExists();
+            return queue;
+        }
+
+        public List<String> GetCloudQueue()
+        {
+            List<string> lista = new List<string>();
+
+            //recuperamos la lista de todas las colas creadas
+            IEnumerable<CloudQueue> queue = this.client.ListQueues();
+           foreach(CloudQueue q in queue)
+            {   // Guardamos el nombre de cada cola
+                lista.Add(q.Name) ;
+            }
+            return lista;
+        }
+
+        public void CrearMensaje(String queuename, String mensaje)
+        {
+            //recuperamos la cola
+            CloudQueue queue = this.Createcola(queuename);
+           //añadimos nuestro mensaje a la lista de la cola
+            CloudQueueMessage msj = new CloudQueueMessage(mensaje);
+            queue.AddMessage(msj);
+
+        }
+
+        public List<CloudQueueMessage> GetMensajes(String queuename)
+        {
+            // creamos una lista para todos los mensajes de la cola
+            List<CloudQueueMessage> mensajes = new List<CloudQueueMessage>();
+            // recuperamos la cola
+            CloudQueue queue = this.Createcola(queuename);
+            
+            foreach (CloudQueueMessage msj in queue.GetMessages(32))
+            {  // guardamos los mensajes a la lista
+                mensajes.Add(msj);
+            }
+            return mensajes;
+        }
+
+        public  void EliminarMensajes(String queuename)
+        {
+            // recuperamos la cola
+            CloudQueue queue = this.Createcola(queuename);
+            //elimino la cola
+            queue.Delete();
+        }
+    }
+}
